de0c97e3d2db5a236ae8af6a5c3311d72607d2bc
Admin <reanajb@gmail.com>
2023-12-12 13:38:00 -0500

    half way done

--- main.js
+++ main.js
@@ -23,29 +23,54 @@ const students = [
   'Andy'
 ];
 
-for(let i = 0; i < students.length; i++){
-  console.log(students)
-}
+// for(let i = 0; i < students.length; i++){
+//   console.log(students[i])
+// }
+
 
 // Challenge 2
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
 const grades = [100, 80, 110, 75, 83, 64];
+
+// for(let i = grades.length -1; i >= 0; i--){
+//   console.log(grades[i]);
+// }
 
 
 // Challenge 3
 // Console.log out only the even numbers in the following array.
 const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
 
+// let evenNums = []
+// for(let i= 0; i < positiveNumbers.length; i++){
+//   if(positiveNumbers[i] % 2 === 0){
+//     evenNums.push(positiveNumbers[i]);
+//   }
+// }
+// console.log(evenNums);
+
+
 
 // Challenge 4
 // Console.log out the even numbers in the following array, INCLUDING the negative ones. There should be four total printings.
 const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
+
+// let evenNums = []
+// for(let i = 0; i < mixedSignNumbers.length; i ++){
+//   if(mixedSignNumbers[i] % 2 === 0){
+//     evenNums.push(mixedSignNumbers[i])
+//   }
+// } 
+// console.log(evenNums)
 
 
 // Challenge 5
 // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
 const symmetricalCapitals = ['A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y'];
 
+// for(symmetricalCapitals)
+ 
+// pop(1) & shift(2)
 
 // Challenge 6
 // Add a value (your choice!) to the start of the following array and two values (of your choice) to the end simply by adding them in the following line, and then console.log out each value individually.
@@ -63,13 +88,46 @@ const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
 // Challenge 9
 // Loop through the `students` array from Challenge 1, starting at index 3 and ending at index 10 (INCLUSIVE), logging out each value individually.
 
+// for(let i = 3; i <= 10; i ++){
+//   console.log(students[i])
+//   }
+
+
 
 // Challenge 10
 // Loop through the `students` array from Challenge 1, making a COPY of the array, starting at index 3 and ending at index 10 (INCLUSIVE). This is very similar to string building; create a new array, loop through the old one, and put values from the original in the new one. Then log out each value of the copy indivudally.
 
+// let newStudents = []
+
+// for(let i = 3; i <= 10; i ++){
+//   newStudents.push(students[i])
+//   }
+//   console.log(newStudents)
+
+//   for(let i = 0; i < newStudents.length; i++){
+//     console.log(newStudents[i])
+//   }
+  
+
 
 // Challenge 11
 // Make a COPY of the `students` array *using .slice*, starting at index 3 and ending at index 10 (INCLUSIVE), then log out each value of the copy individually. THEN log out each value of the original array. Notice that the original array didn't get any values taken out of it, because `slice` does NOT modify the original array.
+
+
+// let newStudents = []
+
+// for(let i = 3; i <= 10; i ++){
+//   newStudents.slice(students[i])
+//   }
+//   console.log(newStudents)
+
+//   for(let i = 0; i < newStudents.length; i++){
+//     console.log(newStudents[i])
+//   }
+//   for(let i = 0; i < students.length; i++){
+//     console.log(students[i])
+//   }
+  
 
 
 // Challenge 12
